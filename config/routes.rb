8d9ddb0e2f6c6ea4362da9e7e598f7fb3a050354
Danilo P. Catalan <danilo.e.catalan@gmail.com>
2023-06-10 01:37:01 -0400

    feat: add handler actions to create and update post + test

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get '/health', to: 'health#health'
-  resources :posts, only: %i[index show]
+  resources :posts, only: %i[index show create update]
 end
